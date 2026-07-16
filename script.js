34564f7cff5239825b46fbad0fc24cf460f60545
mahnooriftikhar-dev <mahnooriftikhar.dev@gmail.com>
2026-07-17 04:05:03 +0500

    Added some HTML and JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const clickbutton = document.getElementById("button");
+let result = document.getElementById("result");
+function generate() {
+    if (clickbutton.click) {
+        function getRandomInt(min, max) {
+            let jsRandomNumber = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+            charAt(0). toUpperCase() . slice(1) 
+        }
+        console.log("jsRandomNumber");
+    }
+}
+
+
