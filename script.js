a66d8c29299432612ffe1def5ea9ed69c9e380b0
mahnooriftikhar-dev <mahnooriftikhar.dev@gmail.com>
2026-07-18 16:00:49 +0500

    Added Uppercase Random Pssword Letter in JS

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const clickbutton = document.getElementById("button");
-let result = document.getElementById("result");
+let random_password = document.getElementById("result");
 function generate() {
-            
+    let upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    length = upper_letters.length;
+    let random_index = Math.floor(Math.random()*length);
+    random_index_number = upper_letters[random_index];
+    console.log(random_index_number);
 }
 
 
