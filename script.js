74e136f4383dfb1a989770df7b5b6a014a19cf78
mahnooriftikhar-dev <mahnooriftikhar.dev@gmail.com>
2026-07-19 22:46:29 +0500

    Added comments in JS

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const clickbutton = document.getElementById("button");
 let random_password = document.getElementById("result");
 function generate() {
+    //Creating 1 character UpperCase value for Random Password
     let upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // length of characters
     length = upper_letters.length;
+    //making the characters length random 
     let random_index = Math.floor(Math.random()*length);
+    //checking the 
     random_index_number = upper_letters[random_index];
     console.log(random_index_number);
 }
